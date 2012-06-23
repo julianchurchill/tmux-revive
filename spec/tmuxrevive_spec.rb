29acdfedbf6856766c6c5df03569f780f6f94d7e
Julian Churchill <chewielouie03@gmail.com>
2012-06-23 14:35:24 +0100

    Fixed tmuxrevive unit tests

--- spec/tmuxrevive_spec.rb
+++ spec/tmuxrevive_spec.rb
@@ -6,16 +6,16 @@ describe TmuxRevive do
   it "calls tmux command line to find window title upon save" do
     tmux_command_line = double( "TmuxExecutor" )
     tmux_revive = TmuxRevive.new tmux_command_line
+    tmux_command_line.should_receive( :window_title ) { "" }
 
     tmux_revive.save
 
-    tmux_command_line.should_receive( :window_title ) { "" }
   end
 
   it "stores current tmux window title upon save" do
     tmux_command_line = double( "TmuxExecutor" )
-    tmux_command_line.stub( :window_title ) { "title" }
     tmux_revive = TmuxRevive.new tmux_command_line
+    tmux_command_line.should_receive( :window_title ) { "title" }
 
     tmux_revive.save
 
