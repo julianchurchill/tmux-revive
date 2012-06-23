50acb290e394f558622389e5747f73f92dd87853
Julian Churchill <chewielouie03@gmail.com>
2012-06-23 14:36:30 +0100

    Added constructor arg for TmuxRevive

--- features/save_steps.rb
+++ features/save_steps.rb
@@ -2,13 +2,13 @@ require_relative '../lib/tmuxexecutor'
 require_relative '../lib/tmuxrevive'
 
 Given /^a single tmux window with a title of "(.*?)"$/ do |title|
-  tmux = TmuxExecutor.new
+  @tmux = TmuxExecutor.new
   @title = title
-  tmux.add_window_with_title @title
+  @tmux.add_window_with_title @title
 end
 
 When /^I trigger a session save$/ do
-  @reviver = TmuxRevive.new
+  @reviver = TmuxRevive.new @tmux
   @reviver.save
 end
 
