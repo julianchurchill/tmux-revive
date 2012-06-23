798aeb041e9800bc0c7fa150e67cd65c71467e4f
Julian Churchill <chewielouie03@gmail.com>
2012-06-23 23:35:13 +0100

    Remove before section to replace with memoized vars in 'let' functions

--- spec/tmuxrevive_spec.rb
+++ spec/tmuxrevive_spec.rb
@@ -2,23 +2,22 @@ require_relative '../lib/tmuxrevive'
 require_relative '../lib/tmuxexecutor'
 
 describe TmuxRevive do
-  before do
-    @tmux_command_line = double( "TmuxExecutor" )
-    @tmux_revive = TmuxRevive.new @tmux_command_line
-  end
+
+  let(:tmux_command_line) { double( "TmuxExecutor" ) }
+  let(:tmux_revive) { TmuxRevive.new tmux_command_line }
 
   it "stores current tmux window title upon save" do
-    @tmux_command_line.should_receive( :window_title ) { "title" }
+    tmux_command_line.should_receive( :window_title ) { "title" }
 
-    @tmux_revive.save.window_title.should == "title"
+    tmux_revive.save.window_title.should == "title"
   end
 
   it "restores tmux window title from session on restore" do
     session = TmuxSession.new
     session.window_title = "title"
-    @tmux_command_line.should_receive( :set_window_title ).with( "title" )
+    tmux_command_line.should_receive( :set_window_title ).with( "title" )
 
-    @tmux_revive.restore session
+    tmux_revive.restore session
   end
 
 end
