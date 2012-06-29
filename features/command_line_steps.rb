723af02deab1295b5c38d3bbe67b68caf959c4e1
Julian Churchill <chewielouie03@gmail.com>
2012-06-29 21:45:51 +0100

    Completed cucumber test steps and simplified tmuxrevive script

--- features/command_line_steps.rb
+++ features/command_line_steps.rb
@@ -1,6 +1,6 @@
 
 Before do
-  @session_ids_to_kill = []
+  @session_ids_to_kill = Set.new
 end
 
 After do
@@ -12,14 +12,21 @@ end
 Given /^a running tmux session with a window title of "(.*?)"$/ do |title|
   # -d is detached, -n is window name
   `tmux new-session -d -n #{title} \;`
-  @session_ids_to_kill += [get_last_tmux_session_id]
+  @session_ids_to_kill.add( get_last_tmux_session_id )
 end
 
 Then /^a new real tmux session should be started$/ do
-  pending "check a real tmux session has been started"
-  pending "how do we kill the real tmux session started by tmuxrevive in the successful case, at the end of the scenario"
+  new_session_id = get_last_tmux_session_id
+  @session_ids_to_kill.should_not_include new_session_id
+  @session_ids_to_kill.add( new_session_id )
 end
 
 Then /^the real tmux session window title should be "(.*?)"$/ do |title|
-  pending "check the real tmux session title is correct"
+  real_window_title = get_window_name_for_session( get_last_tmux_session_id )
+  real_window_title.should == title
 end
+
+def get_window_name_for_session id
+  `tmux list-windows -t #{id}`[/^.*?: (.*?) \[/, 1]
+end
+
