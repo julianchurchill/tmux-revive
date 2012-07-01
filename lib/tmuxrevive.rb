1f909a41071a48bf78fa5335f8aa297a59da4ebd
Julian Churchill <chewielouie03@gmail.com>
2012-07-01 17:06:32 +0100

    Added test and implemented for save session file

--- lib/tmuxrevive.rb
+++ lib/tmuxrevive.rb
@@ -2,5 +2,6 @@
 class TmuxRevive
   def save
     Dir.mkdir( "~/.tmuxrevive" )
+    File.open( "~/.tmuxrevive/session.1", 'w' ) { |f| f.write("") }
   end
 end
--- spec/tmuxrevive_spec.rb
+++ spec/tmuxrevive_spec.rb
@@ -6,11 +6,19 @@ describe TmuxRevive do
     it "creates the .tmuxrevive directory if it doesn't exist" do
       t = TmuxRevive.new
       Dir.should_receive( :mkdir ).with( "~/.tmuxrevive" )
+      File.stub( :open )
 
       t.save
     end
 
-    it "saves the session in a numbered file in the .tmuxrevive directory"
+    it "saves the session in a numbered file in the .tmuxrevive directory" do
+      t = TmuxRevive.new
+      Dir.stub( :mkdir )
+      File.should_receive( :open ).with( "~/.tmuxrevive/session.1", 'w' )
+
+      t.save
+    end
+
     it "numbers the session files sequentially from 1"
     it "saves the window title in the session file"
   end
