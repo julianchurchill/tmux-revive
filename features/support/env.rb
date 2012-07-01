a0fa229984204223b642b1ce061e7f255615f0ef
Julian Churchill <chewielouie03@gmail.com>
2012-07-01 23:31:17 +0100

    Fixed tmp/aruba directory creation race condition

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,9 @@ require 'aruba/cucumber'
 Before do
   @original_home = ENV['HOME']
   ENV['HOME'] = "#{File.expand_path(File.dirname(__FILE__) + '/../../tmp/aruba')}"
+  # tmp/aruba doesn't get created till later but we need to set it up now...
+  Dir.mkdir( ENV['HOME'][/(.*?)\/aruba/, 1 ] ) unless Dir.exists? ENV['HOME'][/(.*?)\/aruba/, 1 ]
+  Dir.mkdir( ENV['HOME'] ) unless Dir.exists? ENV['HOME']
   `ln -fs #{@original_home}/.tmux.conf #{ENV['HOME']}/`
 end
 
