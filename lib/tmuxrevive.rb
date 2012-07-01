6a1fe2e301fde8abb6e038fe6890648b0b66b576
Julian Churchill <chewielouie03@gmail.com>
2012-07-01 17:17:50 +0100

    Added test and implemented sequential numbering of saved session files

--- lib/tmuxrevive.rb
+++ lib/tmuxrevive.rb
@@ -1,7 +1,12 @@
 
 class TmuxRevive
+  def initialize
+    @last_id = 1
+  end
+
   def save
     Dir.mkdir( "~/.tmuxrevive" )
-    File.open( "~/.tmuxrevive/session.1", 'w' ) { |f| f.write("") }
+    File.open( "~/.tmuxrevive/session.#{@last_id}", 'w' ) { |f| f.write("") }
+    @last_id += 1
   end
 end
--- spec/tmuxrevive_spec.rb
+++ spec/tmuxrevive_spec.rb
@@ -14,12 +14,25 @@ describe TmuxRevive do
     it "saves the session in a numbered file in the .tmuxrevive directory" do
       t = TmuxRevive.new
       Dir.stub( :mkdir )
-      File.should_receive( :open ).with( "~/.tmuxrevive/session.1", 'w' )
+      File.should_receive( :open ).with( /~\/\.tmuxrevive\/session\.[\d]/, 'w' )
 
       t.save
     end
 
-    it "numbers the session files sequentially from 1"
+    it "numbers the session files sequentially from 1" do
+      t = TmuxRevive.new
+      Dir.stub( :mkdir )
+      File.should_receive( :open ).with( /\.1$/, 'w' )
+      t.save
+
+      File.should_receive( :open ).with( /\.2$/, 'w' )
+      t.save
+
+      File.should_receive( :open ).with( /\.3$/, 'w' )
+      t.save
+    end
+
+    it "numbers the session files sequentially after the last saved session"
     it "saves the window title in the session file"
   end
 
