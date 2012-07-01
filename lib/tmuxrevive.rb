4ce83b23c3fbf150936dd3e1f12e83006c9b9f97
Julian Churchill <chewielouie03@gmail.com>
2012-07-01 19:54:14 +0100

    Added test and implemented numbering of sessions after highest numbered saved session

--- lib/tmuxrevive.rb
+++ lib/tmuxrevive.rb
@@ -1,12 +1,16 @@
 
 class TmuxRevive
-  def initialize
-    @last_id = 1
-  end
-
   def save
     Dir.mkdir( "~/.tmuxrevive" )
-    File.open( "~/.tmuxrevive/session.#{@last_id}", 'w' ) { |f| f.write("") }
-    @last_id += 1
+    File.open( "~/.tmuxrevive/session.#{find_next_free_session_id}", 'w' ) { |f| f.write("") }
+  end
+
+  def find_next_free_session_id
+    next_id = 1
+    Dir.foreach( "~/.tmuxrevive" ) do |f|
+      id = f[/^session\.(\d+)$/, 1].to_i
+      next_id = id + 1 if id >= next_id
+    end
+    next_id
   end
 end
--- spec/tmuxrevive_spec.rb
+++ spec/tmuxrevive_spec.rb
@@ -3,36 +3,45 @@ require_relative '../lib/tmuxrevive'
 describe TmuxRevive do
 
   context "#save" do
-    it "creates the .tmuxrevive directory if it doesn't exist" do
-      t = TmuxRevive.new
-      Dir.should_receive( :mkdir ).with( "~/.tmuxrevive" )
+    before(:each) do
+      @tmuxrevive = TmuxRevive.new
+      Dir.stub( :mkdir )
+      Dir.stub( :foreach )
       File.stub( :open )
+    end
 
-      t.save
+    it "creates the .tmuxrevive directory if it doesn't exist" do
+      Dir.should_receive( :mkdir ).with( "~/.tmuxrevive" )
+
+      @tmuxrevive.save
     end
 
     it "saves the session in a numbered file in the .tmuxrevive directory" do
-      t = TmuxRevive.new
-      Dir.stub( :mkdir )
       File.should_receive( :open ).with( /~\/\.tmuxrevive\/session\.[\d]/, 'w' )
 
-      t.save
+      @tmuxrevive.save
     end
 
     it "numbers the session files sequentially from 1" do
-      t = TmuxRevive.new
-      Dir.stub( :mkdir )
       File.should_receive( :open ).with( /\.1$/, 'w' )
-      t.save
+      @tmuxrevive.save
 
+      Dir.stub( :foreach ).with( "~/.tmuxrevive" ).and_yield( "session.1" )
       File.should_receive( :open ).with( /\.2$/, 'w' )
-      t.save
+      @tmuxrevive.save
 
+      Dir.stub( :foreach ).with( "~/.tmuxrevive" ).and_yield( "session.1" ).and_yield( "session.2" )
       File.should_receive( :open ).with( /\.3$/, 'w' )
-      t.save
+      @tmuxrevive.save
     end
 
-    it "numbers the session files sequentially after the last saved session"
+    it "numbers the session files sequentially after the highest numbered saved session" do
+      Dir.should_receive( :foreach ).with( "~/.tmuxrevive" ).and_yield( "session.4" ).and_yield( "session.2" )
+      File.should_receive( :open ).with( /\.5$/, 'w' )
+
+      @tmuxrevive.save
+    end
+
     it "saves the window title in the session file"
   end
 
