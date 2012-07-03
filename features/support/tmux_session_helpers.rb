f779d444a6b33346a6a3f49bdec2e55e5a1d597c
Julian Churchill <chewielouie03@gmail.com>
2012-07-03 20:04:46 +0100

    Added helpers for sending commands, exiting sessions and getting window names

--- features/support/tmux_session_helpers.rb
+++ features/support/tmux_session_helpers.rb
@@ -10,6 +10,21 @@ module TmuxSessionHelpers
     end
     all_ids
   end
+
+  def get_window_name_for_session id
+    `tmux list-windows -t #{id}`[/^.*?: (.*?) \[/, 1]
+  end
+
+  def exit_session id
+    send_command id, "exit"
+    `TMUX= tmux attach -t #{id} \;`
+  end
+
+  def send_command id, keys
+    `tmux send-keys -t #{id} \"#{keys}\"\;`
+    `tmux send-keys -t #{id} Enter \;`
+  end
+
 end
 World(TmuxSessionHelpers)
 
